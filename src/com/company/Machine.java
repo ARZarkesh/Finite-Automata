0be371ad07bbedc3c26f3b5bbd255c0210c22022
Amirreza Zarkesh <amirreza.zarkesh.23@gmail.com>
2021-07-01 22:42:21 +0430

    minimize DFA

--- src/com/company/Machine.java
+++ src/com/company/Machine.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.Exception.InvalidCountOfInitialStatesException;
 import com.company.Exception.InvalidInputStringException;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Machine {
@@ -168,7 +169,66 @@ public class Machine {
         return dfa;
     }
 
+    private boolean isDFA() {
+        for (State state : this.states) {
+            for (char letter : this.alphabet.getLetters()) {
+                if (getNextStates(state, letter).size() != 1) return false;
+            }
+        }
+
+        return true;
+    }
+
+    public boolean isDistinguishable(State s1, State s2) {
+        if (!s1.isFinal && s2.isFinal) return true;
+        if (s1.isFinal && !s2.isFinal) return true;
+
+        for (char letter : this.alphabet.getLetters()) {
+            if (getNextStates(s1, letter).get(0) != getNextStates(s2, letter).get(0))
+                return true;
+        }
+
+        return false;
+    }
+
+   public Machine minimizeDFA() {
+        if (!isDFA()) {
+            System.out.println("The machine is not a DFA");
+            return null;
+        }
+        for (int i = 0; i < this.states.size() - 1; i++) {
+            for (int j = i + 1; j < this.states.size(); j++) {
+                State s1 = this.states.get(i);
+                State s2 = this.states.get(j);
+                if (!isDistinguishable(s1, s2)) {
+                    s1.name = s1.name + s2.name;
+                    this.states.remove(s2);
+                    for (int k = 0; k < this.functions.size(); k++) {
+                        TransitionFunction function = this.functions.get(k);
+                        if (function.source == s2) {
+                            this.functions.remove(function);
+                            k--;
+                        }
+                        else if (function.target == s2) {
+                            function.target = s1;
+                        }
+                    }
+                }
+            }
+        }
+
+        return this;
+   }
+
     public LinkedList<TransitionFunction> getFunctions() {
         return functions;
+    }
+
+    private void findAllPairs() {
+        for (int i = 0; i < this.states.size(); i++) {
+            for (int j = i; j < this.states.size(); j++) {
+
+            }
+        }
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,31 +7,51 @@ public class Main {
     public static void main(String[] args) {
         State q0 = new State("q0", false, true);
         State q1 = new State("q1", false, false);
-        State q2 = new State("q2", false, false);
-        State q3 = new State("q3", true, false);
+        State q2 = new State("q2", true, false);
+        State q3 = new State("q3", false, false);
+        State q4 = new State("q4", true, false);
 
-        TransitionFunction f1 = new TransitionFunction(q0, 'a', q1);
-        TransitionFunction f2 = new TransitionFunction(q0, 'b', q2);
-        TransitionFunction f3 = new TransitionFunction(q2, 'λ', q3);
+        TransitionFunction f1 = new TransitionFunction(q0, '0', q1);
+        TransitionFunction f2 = new TransitionFunction(q0, '1', q3);
+        TransitionFunction f3 = new TransitionFunction(q1, '0', q2);
+        TransitionFunction f4 = new TransitionFunction(q1, '1', q4);
+        TransitionFunction f5 = new TransitionFunction(q2, '0', q1);
+        TransitionFunction f6 = new TransitionFunction(q2, '1', q4);
+        TransitionFunction f7 = new TransitionFunction(q3, '0', q2);
+        TransitionFunction f8 = new TransitionFunction(q3, '1', q4);
+        TransitionFunction f9 = new TransitionFunction(q4, '0', q4);
+        TransitionFunction f10 = new TransitionFunction(q4, '1', q4);
 
         LinkedList<State> states = new LinkedList<>();
         states.push(q0);
         states.push(q1);
         states.push(q2);
         states.push(q3);
+        states.push(q4);
 
-        Alphabet alphabet = new Alphabet(new char[]{'a', 'b'});
+        Alphabet alphabet = new Alphabet(new char[]{'0', '1'});
         LinkedList<TransitionFunction> functions = new LinkedList<>();
-        functions.push(f1);
-        functions.push(f2);
-        functions.push(f3);
+        functions.add(f1);
+        functions.add(f2);
+        functions.add(f3);
+        functions.add(f4);
+        functions.add(f5);
+        functions.add(f6);
+        functions.add(f7);
+        functions.add(f8);
+        functions.add(f9);
+        functions.add(f10);
 
         try {
             Machine machine = new Machine(states, alphabet, functions);
+            Machine minimized = machine.minimizeDFA();
 //            machine.testString("a");
-            Machine dfa = machine.convertToDFA();
-            for (TransitionFunction f : dfa.getFunctions()) {
-                f.show();
+//            Machine dfa = machine.convertToDFA();
+//            for (TransitionFunction f : dfa.getFunctions()) {
+//                f.show();
+//            }
+            for (TransitionFunction function : minimized.getFunctions()) {
+                function.show();
             }
         } catch (Exception e) {
             e.printStackTrace();
